e343153438663fc8eb756a0e216587a230668e00
ksdivya <srinivasan1964@hotmail.com>
2017-09-27 19:12:10 +0530

    Added menu.html

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function revealMessage() {
+ document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+function clearform()
+{
+ document.getElementById("fname").value="";
+ document.getElementById("lname").value="";
+document.getElementById("email").value="";
+}
